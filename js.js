ac53d03b45e3a64861e45403fa8caddc7f34001a
bijay55-cmd <71406824+bijay55-cmd@users.noreply.github.com>
2021-07-04 12:00:18 +0100

    Update js.js

--- js.js
+++ js.js
@@ -32,3 +32,19 @@ slider.addEventListener('mousemove', (e) => {
   const walk = (x - startX) * 3; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
 });
+//accordion 
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function(question){
+const btn = question.querySelector(".question-btn");
+btn.addEventListener('click', function(){
+
+   questions.forEach(function(item){
+      if(item !== question){
+         item.classList.remove("show-text");
+      }
+   });
+   question.classList.toggle("show-text");
+})
+});
+
